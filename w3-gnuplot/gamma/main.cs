26c9b83bd736179d36a8e623e1523f3e6a95b081
Henrik-AU <henrik-home@DESKTOP-ESE1DTB.localdomain>
2020-03-04 17:06:40 +0100

    Finished gnuplot exercise A

--- w3-gnuplot/gamma/main.cs
+++ w3-gnuplot/gamma/main.cs
@@ -4,10 +4,10 @@ class main{
 	static void Main(){
 	// We create a spacing for our x-values, and a small epsilon shift for the endpoints.
 	// The epsilon shift helps us avoid problematic x-values (integer numbers)
-	double eps = 1.0/64;
-	double dx = 1.0/32;
+	double eps = 1.0/128;
+	double dx = 1.0/64;
 
-	for(double x = -4+eps;x <=4-eps; x+=dx){
+	for(double x = -5+eps;x <=5-eps; x+=dx){
 		// We write out the values of gamma at the x-points so we can later plot them with Gnuplot
 		WriteLine("{0,6:f3} {1,16:f8}", x, math.gamma(x));
 
