41d39c00af99480f510fe8c4129feaf1781f77ee
Henrik-AU <201606245@post.au.dk>
2020-03-14 14:20:33 +0100

    Exercise orbit B - part i is faulty

--- w5-orbit/B/main.cs
+++ w5-orbit/B/main.cs
@@ -6,29 +6,58 @@ using System.Collections.Generic;
 class main{
 	static void Main(){
 		
-		double eps;
 
+		// Part I)
+		vector ystart = new vector(1, 0);
+		double eps = 0;
+		double xa = 0;
+		double xb = 4*PI;
+		string filepathI = "outBpartI.txt";
+
+		// To avoid code repetition we call a function orbitsolver for each set of parameters
+		orbitsolver(eps, xa, xb, ystart, filepathI);
+		
+		
+		// Part II)
+		ystart[1] = -0.5;
+		string filepathII = "outBpartII.txt";
+		orbitsolver(eps, xa, xb, ystart, filepathII);
+		
+		
+		// Part III)
+		eps = 0.01;
+		xb = 15*PI;
+		string filepathIII = "outBpartIII.txt";
+		orbitsolver(eps, xa, xb, ystart, filepathIII);
+
+
+}
+
+	public static vector orbitsolver(double eps, double xa, double xb, vector ystart, string filepath){
+		
 		// Create a function for the planet equation
 		Func<double, vector, vector> planetmotion = delegate(double x, vector y){
 			return new vector(y[1], 1 + eps*y[0]*y[0] - y[0]);
 		};
-		
-		// Setup region of interest, start conditions and prepare lists for x and y data
-		double xa = 0;
-		double xb = 6*PI;
-				
-		//vector ystart = new vector(0.5, 0);
+
 		List<double> xs = new List<double>();
 		List<vector> ys = new List<vector>();
 
-		// Solve the differential equation
-		ode.rk23(planetmotion,xa,ystart,xb,xlist:xs,ylist:ys);
+		vector odeout = ode.rk23(planetmotion,xa,ystart,xb,xlist:xs,ylist:ys);
+
+		// Create a streamwriter connected to the wanted output file
+		var outfile = new System.IO.StreamWriter(filepath,append:true);
 		
+		// Print out the data to the file (we don't need to print the first derivative)
+		for(int i=0; i<xs.Count; i++)
+			outfile.WriteLine("{0:f8}\t{1:f8}", xs[i], ys[i][0]);
+		
+		outfile.Close();
 
-		// Print out the data to a file
-		//for(int i=0; i<xs.Count; i++)
-		//	WriteLine("{0:f8}\t{1:f8}\t{2:f8}", xs[i], ys[i][0], ys[i][1]);
+		return odeout;
+
+	}	
+
 
 }
 
-}
