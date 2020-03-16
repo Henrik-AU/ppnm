670dad63617350efdbd4e195bb561979aaf3c96b
Henrik-AU <henrik-home@DESKTOP-ESE1DTB.localdomain>
2020-03-16 19:08:07 +0100

    Started problem interpolation

---
+++ problems/interpolation/A/linspline.cs
@@ -0,0 +1,69 @@
+using System.Diagnostics;
+
+public class linispline{
+
+
+	public static int binsearch(vector x, double z)
+		{/* locates the interval for z by bisection */ 
+		int i=0, j=x.size-1;
+		while(j-i>1){
+			int mid=(i+j)/2;
+			if(z>x[mid]) i=mid; else j=mid;
+			}
+		return i;
+	}
+
+
+	public linspline(double[] xs, double[] ys){
+		int n = xs.Length;
+		// Check if the dimension of the x and y arrays are equal - otherwise display
+		// an error
+		Trace.Assert(ys.Length = n,"The dimension of the x and y arrays are not equal.");
+		
+		x = new double[n];
+		y = new double[n];
+		dx = new double[n-1];
+		p = new double[n-1];
+
+		for(int i=0; i<n; i++){
+			x[i] = xs.[i];
+			y[i] = ys.[i];
+		}
+	
+		for(int=0; i<n; i++){
+			dx[i] = x[i+1] - x[i];
+			Trace.Assert(dx[i] > 0, "The x-array is not ordered from lowest to highest.")
+		}
+
+		for(int=0; i<n; i++){
+			p[i] = (y[i+1] - y[i])/dx[i];
+		}
+
+
+	}
+
+	public double eval(double z){
+		Trace.Assert(z >= x[0] && z<=x[x.Length-1], "The z-value is outside the valid x region.");
+		int i = binsearch(x, z);
+		dx = z - x[i];
+		return y[i] + p[i]*dx;
+	}
+
+	public double integrate(double z){
+		Trace.Assert(z >= x[0] && z<=x[x.Length-1], "The z-value is outside the valid x region.");
+		int i = binsearch(x, z);
+		double sum;
+		double dx;
+		// We integrate S_i between each set of points individually and add the results
+		for(int k=0; k<i; k++){
+			dx = x[k+1] - x[k];
+			sum+= dx*(y[k] + p[k]*dx/2 - p[k]*dx[k]);
+		}
+		// At last we add the part from the interval which z lies in.
+		dx = z - x[i];	
+		sum+= dx*(y[i] + p[i]*dx/2 - p[i]*dx[i]);
+		
+		return sum;
+	}
+
+}
---
+++ problems/interpolation/A/main.cs
@@ -0,0 +1,9 @@
+using static System.Console;
+using static math;
+
+class main{
+	public static void Main(){
+
+
+}
+}
