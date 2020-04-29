7c42ff43d1e90a3282d1a17d64efb19876f9f12a
Henrik-AU <henrik-home@DESKTOP-ESE1DTB.localdomain>
2020-04-29 16:09:36 +0200

    Updated downhill simplex algorithm

--- problems/minimization/CAnimation/dsimplex.cs
+++ problems/minimization/CAnimation/dsimplex.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using static System.Console;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -15,26 +14,15 @@ public partial class minimization{
 
 	int minIndex;
 	int maxIndex;
-
-	StreamWriter printAni = new StreamWriter("animation.txt");
+		
+	// Calculate the initial function values for the points
+	vector fValues = new vector(n);
+	for(int i=0; i<n; i++){
+		fValues[i] = f(points[i]);
+	}
 
 	while(true){
 		nsteps++;
-
-		// Print out the vectors for a fancy animation plot
-		if(nsteps < 120){
-			for(int i=0; i<n; i++){
-				printAni.WriteLine("{0}\t{1}\t0", points[i][0], points[i][1]);
-			}
-			printAni.WriteLine("{0}\t{1}\t0", points[0][0], points[0][1]);
-			printAni.Write("\n\n");		
-		}
-
-		// Calculate the function values for the points
-		vector fValues = new vector(n);
-		for(int i=0; i<n; i++){
-			fValues[i] = f(points[i]);
-		}
 
 		// Find the index of the vector giving the highest function value.
 		maxIndex = findMaxIndex(fValues);
@@ -49,24 +37,31 @@ public partial class minimization{
 		// minimum. We do not know if the points come ordered such that this is actually
 		// the distance along the "circumference" of the simplex, but it should get smaller
 		// and smaller during the runs, and thus converge at some point, so it should work.
-		double distances = 0;
+		double distance = 0;
 		for(int i=0; i<n-1; i++){
-			distances += (points[i]-points[i+1]).norm();
+			distance += (points[i]-points[i+1]).norm();
 		}
-		if(distances < eps){
+
+		// Divide the circumference distance with n-1, which is the amount of arguments the
+		// objective function takes. Thus the divergence criterium should be somewhat the 
+		// same no matter the dimension of the objective function.
+		distance = distance/(n-1);
+		if(distance < eps){
 			Error.WriteLine("Steps: {0}\n\n", nsteps);
 			break;
 		}
 		
 		
 		// Now it is time to attempt to take a step
+		// Vector highPoint and lowPoint introduced only for readability of the code.
+		// For better optimization they can be omitted and points[maxIndex] or
+		// points[minIndex] used directly instead.
 		vector highPoint = points[maxIndex];
 		vector lowPoint = points[minIndex];
 		double fHigh = fValues[maxIndex];
 		double fLow = fValues[minIndex];
 
 		// First we try reflection
-		//vector reflected = new vector(n-1);
 		vector reflected = centroid + (centroid - highPoint);
 		double fRef = f(reflected); 
 
@@ -77,10 +72,12 @@ public partial class minimization{
 			if(fExp < fRef){
 				// Accept expansion instead of highest point
 				points[maxIndex] = expanded;
+				fValues[maxIndex] = fExp;
 				Error.WriteLine("Expanding");
 			}else{
 				// Accept reflection instead of highest point
 				points[maxIndex] = reflected;
+				fValues[maxIndex] = fRef;
 				Error.WriteLine("Reflected - better than lowest value");
 
 			}			
@@ -90,16 +87,17 @@ public partial class minimization{
 			if(fRef < fHigh){
 				// Accept reflection instead of highest point
 				points[maxIndex] = reflected;
+				fValues[maxIndex] = fRef;
 				Error.WriteLine("Reflected - better than highest value");
 			}else{
-				// Try contraction - I have found the 1/1.75 factor to be effective
-				// 1/2 did not work at all - the simplex shrunk too quickly which
-				// gave issues with the simplex getting stuck in a wrong spot
-				vector contracted = centroid + (centroid - highPoint)/1.75;
+				// Try contraction
+				vector contracted = centroid + (highPoint - centroid)/2;
 				double fCon = f(contracted);
+				
 				if(fCon < fHigh){
 					// Accept contraction instead of highest point
 					points[maxIndex] = contracted;
+					fValues[maxIndex] = fCon;
 					Error.WriteLine("Contracted");
 				}else{
 					Error.WriteLine("Reduced");
@@ -107,6 +105,9 @@ public partial class minimization{
 					for(int i=0; i<n; i++){
 						if(i!=minIndex){
 							points[i] = (points[i] + points[minIndex])/2;
+							// Calculate new function values for the
+							// moved points
+							fValues[i] = f(points[i]);
 						}
 					}
 				}
@@ -116,9 +117,7 @@ public partial class minimization{
 
 	}
 
-	printAni.Close();
-
-	// Return the vector that give the current lowest point
+	// Return the vector that give the lowest point
 	vector result = points[minIndex];
 	return result;
 	}
--- problems/minimization/CAnimation/dsimplexBackup.cs
+++ problems/minimization/CAnimation/dsimplexBackup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using static System.Console;
 using System.Diagnostics;
 using System.Collections.Generic;
@@ -14,15 +15,26 @@ public partial class minimization{
 
 	int minIndex;
 	int maxIndex;
-		
-	// Calculate the initial function values for the points
-	vector fValues = new vector(n);
-	for(int i=0; i<n; i++){
-		fValues[i] = f(points[i]);
-	}
+
+	StreamWriter printAni = new StreamWriter("animation.txt");
 
 	while(true){
 		nsteps++;
+
+		// Print out the vectors for a fancy animation plot
+		if(nsteps < 120){
+			for(int i=0; i<n; i++){
+				printAni.WriteLine("{0}\t{1}\t0", points[i][0], points[i][1]);
+			}
+			printAni.WriteLine("{0}\t{1}\t0", points[0][0], points[0][1]);
+			printAni.Write("\n\n");		
+		}
+
+		// Calculate the function values for the points
+		vector fValues = new vector(n);
+		for(int i=0; i<n; i++){
+			fValues[i] = f(points[i]);
+		}
 
 		// Find the index of the vector giving the highest function value.
 		maxIndex = findMaxIndex(fValues);
@@ -37,31 +49,24 @@ public partial class minimization{
 		// minimum. We do not know if the points come ordered such that this is actually
 		// the distance along the "circumference" of the simplex, but it should get smaller
 		// and smaller during the runs, and thus converge at some point, so it should work.
-		double distance = 0;
+		double distances = 0;
 		for(int i=0; i<n-1; i++){
-			distance += (points[i]-points[i+1]).norm();
+			distances += (points[i]-points[i+1]).norm();
 		}
-
-		// Divide the circumference distance with n-1, which is the amount of arguments the
-		// objective function takes. Thus the divergence criterium should be somewhat the 
-		// same no matter the dimension of the objective function.
-		distance = distance/(n-1);
-		if(distance < eps){
+		if(distances < eps){
 			Error.WriteLine("Steps: {0}\n\n", nsteps);
 			break;
 		}
 		
 		
 		// Now it is time to attempt to take a step
-		// Vector highPoint and lowPoint introduced only for readability of the code.
-		// For better optimization they can be omitted and points[maxIndex] or
-		// points[minIndex] used directly instead.
 		vector highPoint = points[maxIndex];
 		vector lowPoint = points[minIndex];
 		double fHigh = fValues[maxIndex];
 		double fLow = fValues[minIndex];
 
 		// First we try reflection
+		//vector reflected = new vector(n-1);
 		vector reflected = centroid + (centroid - highPoint);
 		double fRef = f(reflected); 
 
@@ -72,12 +77,10 @@ public partial class minimization{
 			if(fExp < fRef){
 				// Accept expansion instead of highest point
 				points[maxIndex] = expanded;
-				fValues[maxIndex] = fExp;
 				Error.WriteLine("Expanding");
 			}else{
 				// Accept reflection instead of highest point
 				points[maxIndex] = reflected;
-				fValues[maxIndex] = fRef;
 				Error.WriteLine("Reflected - better than lowest value");
 
 			}			
@@ -87,17 +90,14 @@ public partial class minimization{
 			if(fRef < fHigh){
 				// Accept reflection instead of highest point
 				points[maxIndex] = reflected;
-				fValues[maxIndex] = fRef;
 				Error.WriteLine("Reflected - better than highest value");
 			}else{
 				// Try contraction
 				vector contracted = centroid + (highPoint - centroid)/2;
 				double fCon = f(contracted);
-				
 				if(fCon < fHigh){
 					// Accept contraction instead of highest point
 					points[maxIndex] = contracted;
-					fValues[maxIndex] = fCon;
 					Error.WriteLine("Contracted");
 				}else{
 					Error.WriteLine("Reduced");
@@ -105,9 +105,6 @@ public partial class minimization{
 					for(int i=0; i<n; i++){
 						if(i!=minIndex){
 							points[i] = (points[i] + points[minIndex])/2;
-							// Calculate new function values for the
-							// moved points
-							fValues[i] = f(points[i]);
 						}
 					}
 				}
@@ -117,7 +114,9 @@ public partial class minimization{
 
 	}
 
-	// Return the vector that give the lowest point
+	printAni.Close();
+
+	// Return the vector that give the current lowest point
 	vector result = points[minIndex];
 	return result;
 	}
