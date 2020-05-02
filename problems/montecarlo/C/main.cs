d96aa014e9d4f3cfb195976a638ab08a4393598b
Henrik-AU <henrik-home@DESKTOP-ESE1DTB.localdomain>
2020-05-02 18:06:26 +0200

    Small update

--- problems/montecarlo/C/main.cs
+++ problems/montecarlo/C/main.cs
@@ -6,39 +6,33 @@ using static System.Math;
 class main{
 	static void Main(){
 
-		// We will integrate a relatively simple function (x^2 + y^2 + z^2) many times
-		// using different amount of point evaluations in the volume
+		// TO BE IMPLEMENTED	
+
+		// To test the stratified Monte Carlo algorithm, we use a function with stronger
+		// variations in some regions than others.
 
 		// Lower and upper corner point of volume
-		vector a = new vector(0.0, 0.0, 0.0);
-		vector b = new vector(3, 3, 3);
+		//vector a = new vector(0.0, 0.0, 0.0);
+		//vector b = new vector(3, 3, 3);
 
+		/*
 		Func<vector, double> f = delegate(vector x){
 			// This function is on a simple rectangular volume
-			double result = x[0]*x[0] + x[1]*x[1] + x[2]*x[2];
+			double result = ;
 			return result;
 		};
-		
-		double expected = 243;
+		*/
 
-		// We run it once outside a loop so we can save the scaling factor for the
-		// O(1/sqrt(N)) part.
+		// double expected = ;
+
+		// Amount of random points to be evaluated at first, and the wanted accuracy of the
+		// result
 		int N = 10000;
-		vector estimate = montecarlo.plainmc(f, a, b, N);
-		double scaling = Sqrt(N)*estimate[1];
+		double acc = 1e-3;
 
-		WriteLine("{0}\t{1}\t{2}\t{3}",
-		N, estimate[1], Abs(expected - estimate[0]), scaling*1/Sqrt(N));
-
-		// We run the rest in a loop and print out the data
-		for(int n = 20000; n<300000; n+=10000){
-			estimate = montecarlo.plainmc(f, a, b, n);
-
-			// Save the error estimate and actual error along with the amount of function
-			// evaluations N and O(1/sqrt(N)) scaled
-			WriteLine("{0}\t{1}\t{2}\t{3}",
-			n, estimate[1], Abs(expected - estimate[0]), scaling*1/Sqrt(n));
-		}
+		vector estimate = montecarlo.mcStrat(f, a, b, N);
+		
+		// Print out results
 
 	} // end Main function
 
